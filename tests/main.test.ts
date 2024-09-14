92d5f9d8ddf0cac6cc78655f4e02d3edd164aecd
Catalin Costache <costache@adobe.com>
2024-09-14 15:39:47 +0300

    update tests

--- tests/main.test.ts
+++
@@ -1,7 +0,0 @@
-import { getArcTabsStrings } from "../main";
-
-describe('testing main file', () => {
-    test('should return all tabs', () => {
-      getArcTabsStrings("arctabs.json")
-    });
-  });
